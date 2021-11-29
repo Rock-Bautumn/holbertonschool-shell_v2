e15c1d6542d341da4b8495514e342ebc17868df6
AJS9091 <ashtonjamess@live.com>
2021-11-29 09:21:53 -0800

    Adding get_input.c

---
+++ get_input.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+char * get_input() {
+	char *buff;
+	size_t n = 1;
+	ssize_t check;
+
+	buff = malloc(sizeof(char) * 1);
+	check = (getline(&buff, &n, stdin));
+
+	if (check == -1)
+	{
+		printf("error\n");
+		return(NULL);
+	}
+
+	return(buff);
+}

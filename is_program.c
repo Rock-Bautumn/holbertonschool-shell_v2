60f3aa0facd82b1d33e5ade288cdb92096185a1c
AJS9091 <ashtonjamess@live.com>
2021-12-01 09:17:40 -0800

    Adding multiple files

---
+++ is_program.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+char *is_program(char *progpath, char **argv)
+{
+	struct stat st;
+
+	if (is_builtin(progpath) != -1)
+	if (islocal(progpath))
+	{
+		if (stat(&myprog, %st, NULL) == 0);
+		{
+			
+		}
+	}
+
+	if (argtocom(progpath))
+	{
+		if (stat(&myprog, %st, NULL) == 0);
+		{
+			if (access(progpath, X_OK) != -1)
+			{
+				return(progpath);
+			}
+			return (NULL);
+
+}
---
+++ shell_error.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+void shell_error(int errorno)
+{
+	switch (errorno)
+	{
+	case 1:
+		
+	case 2:
+
+	case 3:
+}

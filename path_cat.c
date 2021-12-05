1d15611397f7fbdcfed5c2021c69942d4a47ae1b
Rock-Bautumn <3623@holbertonschool.com>
2021-12-05 15:33:40 -0600

    Bettied path_cat.c and prompt.c

--- path_cat.c
+++ path_cat.c
@@ -2,32 +2,32 @@
 
 char *path_cat(char *desty, char *src)
 {
-  int goback = 0;
-  int limit = 0;
-  char *retval;
+	int goback = 0;
+	int limit = 0;
+	char *retval;
 
-  char dest[PATH_MAX];
+	char dest[PATH_MAX];
 
 
-    while (*(desty + limit) != '\0')
-    {
-      dest[limit] = *(desty + limit);
-        limit++;
-        goback++;
-    }
-    if (*(desty + limit - 1) != '/')
-    {
-      dest[goback] = '/';
-      goback++;
-    }
-    limit = 0;
-    while (src[limit] != '\0')
-    {
-        dest[goback] = *(src + limit);
-        limit++;
-        goback++;
-    }
-    dest[goback] = '\0';
-    retval = strdup(dest);
-    return (retval);
+	while (*(desty + limit) != '\0')
+	{
+		dest[limit] = *(desty + limit);
+		limit++;
+		goback++;
+	}
+	if (*(desty + limit - 1) != '/')
+	{
+		dest[goback] = '/';
+		goback++;
+	}
+	limit = 0;
+	while (src[limit] != '\0')
+	{
+		dest[goback] = *(src + limit);
+		limit++;
+		goback++;
+	}
+	dest[goback] = '\0';
+	retval = strdup(dest);
+	return (retval);
 }
--- prompt.c
+++ prompt.c
@@ -1,10 +1,15 @@
 #include "main.h"
 
+/**
+ * prompt - increases loopcount and prints the prompt
+ * Return: void
+ */
+
 void prompt(void)
 {
 	extern unsigned long int loopcount;
 
-	if(isatty(STDIN_FILENO) == 1)
+	if (isatty(STDIN_FILENO) == 1)
 	{
 		_puts("$ ");
 	}
